fa6c9c7125f377305c9919f311c45d6b0dfc4442
Henry Migo <henryeamigo@gmail.com>
2020-03-17 13:42:25 +0000

    Cleaned up tests

--- src/app/components/navigation/navigation.component.spec.ts
+++ src/app/components/navigation/navigation.component.spec.ts
@@ -1,5 +1,6 @@
 /* tslint:disable:no-unused-variable */
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
 import { NavigationComponent } from './navigation.component';
 
 describe('NavigationComponent', () => {
