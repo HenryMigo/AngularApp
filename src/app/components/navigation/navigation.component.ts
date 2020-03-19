0db6142704a77b693185638b0ffac8d45e15be81
Henry Migo <henryeamigo@gmail.com>
2020-03-19 21:55:39 +0000

    Added font awesome and changed font

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,11 +1,14 @@
 import { Component, OnInit } from '@angular/core';
 
+import { faLightbulb } from '@fortawesome/free-regular-svg-icons';
+
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.sass']
 })
 export class NavigationComponent implements OnInit {
+  faLightbulb = faLightbulb;
 
   constructor() { }
 
