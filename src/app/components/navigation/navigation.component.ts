c44c0bc0c1c0c422dc5fa4dc7adc244583817c83
Henry Migo <henryeamigo@gmail.com>
2020-03-22 19:11:08 +0000

    renamed to use scss instead of sass

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -5,7 +5,7 @@ import { faLightbulb } from '@fortawesome/free-regular-svg-icons';
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
-  styleUrls: ['./navigation.component.sass']
+  styleUrls: ['./navigation.component.scss']
 })
 export class NavigationComponent implements OnInit {
   faLightbulb = faLightbulb;
