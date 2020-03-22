d7b6b9afb1501f44db149ca7a039097bf972d458
Henry Migo <henryeamigo@gmail.com>
2020-03-22 19:47:44 +0000

    Made navigation responsive

--- src/app/components/navigation/navigation.component.ts
+++ src/app/components/navigation/navigation.component.ts
@@ -1,18 +1,21 @@
-import { Component, OnInit } from '@angular/core';
-
-import { faLightbulb } from '@fortawesome/free-regular-svg-icons';
+import { Component } from '@angular/core';
+import { BreakpointObserver, Breakpoints } from '@angular/cdk/layout';
+import { Observable } from 'rxjs';
+import { map, shareReplay } from 'rxjs/operators';
 
 @Component({
   selector: 'app-navigation',
   templateUrl: './navigation.component.html',
   styleUrls: ['./navigation.component.scss']
 })
-export class NavigationComponent implements OnInit {
-  faLightbulb = faLightbulb;
+export class NavigationComponent {
 
-  constructor() { }
+  isHandset$: Observable<boolean> = this.breakpointObserver.observe(Breakpoints.Handset)
+    .pipe(
+      map(result => result.matches),
+      shareReplay()
+    );
 
-  ngOnInit() {
-  }
+  constructor(private breakpointObserver: BreakpointObserver) {}
 
 }
