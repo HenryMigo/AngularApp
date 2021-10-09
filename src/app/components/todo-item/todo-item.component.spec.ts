59053ff581030b6339b9f3e059a22a4abc47c722
Henry Migo <HenryMigo@users.noreply.github.com>
2021-10-09 13:31:45 +0100

    Moved Angular CORE and CLI to v11

--- src/app/components/todo-item/todo-item.component.spec.ts
+++ src/app/components/todo-item/todo-item.component.spec.ts
@@ -1,5 +1,5 @@
 /* tslint:disable:no-unused-variable */
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { TodoItemComponent } from './todo-item.component';
 
@@ -7,7 +7,7 @@ describe('TodoItemComponent', () => {
   let component: TodoItemComponent;
   let fixture: ComponentFixture<TodoItemComponent>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ TodoItemComponent ]
     })
